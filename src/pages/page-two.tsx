9e7e5fa9f1d784996d3b6f8e4516494535f5afa8
Piotr Karczewski <34404855+karczewskiPiotr@users.noreply.github.com>
2022-10-03 11:11:52 +0200

    Update page-two.tsx

--- src/pages/page-two.tsx
+++ src/pages/page-two.tsx
@@ -13,6 +13,7 @@ const SecondPage = () => {
       <Seo title="Page two" />
       <h1>{t("page-two:this-is-second-page")}</h1>
       Changes to pag 2
+      More changes to page 2
       <p>
         {t("page-two:find-me")} <code>src/pages/page-two.tsx</code>
       </p>
